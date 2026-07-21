f424ee8e999c08dd61e0f1220959c98e89b0ccde
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-21 20:35:40 -0300

    feat: add Plano model definition with attributes and default values

---
+++ src/models/plano.model.js
@@ -0,0 +1,27 @@
+import sequelize from "../database/database.js";
+import { DataTypes } from "sequelize";
+
+export const Plano = sequelize.define(Plano,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        trilhaTitulo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //valores esperados: 'diagnostico_gerado' | 'diagnostico_corrigido' | 'progresso_gerado' | 'progresso_corrigido'
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'diagnostico_gerado'
+        }
+    }, {
+        tableName: 'planos',
+        timestamps: true
+    }
+);
+
+export default Plano;
