b3b7e4a85cb4b83072675b743bf15fb78d019e50
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-23 21:51:05 -0300

    feat: add validation for respostas schema in enviarDiagnostica route

--- src/routes/plano.routes.js
+++ src/routes/plano.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import * as controller from '../controllers/plano.controller.js';
 import {autenticar} from '../middlewares/auth.middleware.js';
+import dadosValidador from '../middlewares/dados.validador.js';
+import respostasSchema from '../schemas/respostas.schema.js'
 import { limitadorListarPlanos, limitadorGerarDiagnostica, limitadorGerarProgresso, limitadorEnviarDiagnostica, limitadorEnviarProgresso } from '../config/rateLimit.js'
 
 
@@ -9,7 +11,7 @@ const planoRoutes = Router();
 planoRoutes.get('/', limitadorListarPlanos, autenticar, controller.listar);
 planoRoutes.post('/diagnostica', limitadorGerarDiagnostica, autenticar, controller.gerarDiagnostica);
 planoRoutes.post('/:id/progresso', limitadorGerarProgresso, autenticar, controller.gerarProgresso);
-planoRoutes.post('/:id/diagnostica/enviar', limitadorEnviarDiagnostica, autenticar, controller.enviarDiagnostica);
+planoRoutes.post('/:id/diagnostica/enviar', limitadorEnviarDiagnostica, autenticar, dadosValidador(respostasSchema), controller.enviarDiagnostica);
 planoRoutes.post('/:id/progresso/enviar', limitadorEnviarProgresso, autenticar, controller.enviarProgresso);
 
 export default planoRoutes;
